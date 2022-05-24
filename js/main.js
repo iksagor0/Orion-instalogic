104d0cb9c7991adba3380564670f890a85002d2b
IK Sagor <iksagor0@gmail.com>
2022-05-24 15:01:21 +0600

    Swiper Slider Added on banner and products section

--- js/main.js
+++ js/main.js
@@ -1,96 +1,118 @@
 // ====== NAV-BAR Sticy ======
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('#header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-})
-
-
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("#header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 // OPEN CLOSE SIDE-BAR BY BUTTON
-const sidebarBtn = document.querySelector('.sidebar-btn-area');
-const navItems = document.querySelectorAll("#header #bottom-header .container .navbar ul li a");
+const sidebarBtn = document.querySelector(".sidebar-btn-area");
+const navItems = document.querySelectorAll(
+    "#header #bottom-header .container .navbar ul li a"
+);
 
 const sidebarActive = () => {
-    const header = document.querySelector('.header');
-    header.classList.toggle('sidebar-active');
-}
+    const header = document.querySelector(".header");
+    header.classList.toggle("sidebar-active");
+};
 
-sidebarBtn.addEventListener('click', sidebarActive);
-
+sidebarBtn.addEventListener("click", sidebarActive);
 
 navItems.forEach((item) => {
-    item.addEventListener('click', sidebarActive);
-})
-
-
+    item.addEventListener("click", sidebarActive);
+});
 
 // NAV ITEM ACTIVE when it's on screen
-const sections = document.querySelectorAll('section');
+const sections = document.querySelectorAll("section");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
     let current;
-    sections.forEach(section => {
-        if (pageYOffset >= (section.offsetTop - (section.clientHeight / 3))) {
-            current = section.getAttribute('id');
+    sections.forEach((section) => {
+        if (pageYOffset >= section.offsetTop - section.clientHeight / 3) {
+            current = section.getAttribute("id");
         }
     });
 
-    navItems.forEach(item => {
-        item.classList.remove('nav-active');
+    navItems.forEach((item) => {
+        item.classList.remove("nav-active");
 
         if (item.classList.contains(current)) {
-            item.classList.add('nav-active');
+            item.classList.add("nav-active");
         }
     });
 });
 
+//  ====== BANNER SLIDER ======
+// const slides = document.querySelectorAll('.banner-slider .slide');
+// let counter = 0;
 
+// const next = () => {
 
+//     if (counter <= slides.length - 1) {
+//         slides.forEach((item) => {
+//             item.classList.remove("active");
+//         })
 
-//  ====== HOMEPAGE SLIDER ======
-const slides = document.querySelectorAll('.banner-slider .slide');
-let counter = 0;
+//         slides[counter].classList.add("active");
+//         counter++;
+//     }
+//     if (counter >= slides.length) {
+//         counter = 0;
+//     }
+// }
 
-const next = () => {
+// setInterval(() => {
+//     next();
+// }, 5000);
 
-    if (counter <= slides.length - 1) {
-        slides.forEach((item) => {
-            item.classList.remove("active");
-        })
+const swiper = new Swiper(".banner-slider", {
+    direction: "horizontal",
+    loop: true,
+    speed: 500,
+    autoplay: {
+        delay: 5000,
+    },
+});
 
-        slides[counter].classList.add("active");
-        counter++;
-    }
-    if (counter >= slides.length) {
-        counter = 0;
-    }
-}
+// =========    PRODUCTS SLIDER =========
+// TINY SLIDER
+// const productSlider = tns({
+//     container: ".products-slider",
+//     items: 1,
+//     slideBy: 1,
+//     autoplay: true,
+//     controls: false,
+//     autoplayButtonOutput: false,
+//     autoplayTimeout: 2000,
+//     speed: 1000,
+//     responsive: {
+//         850: {
+//             items: 2,
+//         },
+//         300: {
+//             autoplayTimeout: 1500,
+//             speed: 500,
+//             item: 1,
+//         },
+//     },
+// });
 
-setInterval(() => {
-    next();
-}, 5000);
-
-
-
-
-// =========    TINY SLIDER =========
-const productSlider = tns({
-    container: '.products-slider',
-    items: 1,
-    slideBy: 1,
-    autoplay: true,
-    controls: false,
-    autoplayButtonOutput: false,
-    autoplayTimeout: 2000,
-    speed: 1000,
-    responsive: {
-        850: {
-            items: 2
+var swiperTwo = new Swiper(".products-slider", {
+    // SWIPER SLIDER
+    loop: true,
+    speed: 800,
+    slidesPerView: 2,
+    autoplay: {
+        delay: 500,
+    },
+    spaceBetween: 30,
+    breakpoints: {
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 50,
         },
-        300: {
-            autoplayTimeout: 1500,
-            speed: 500,
-            item: 1
-        }
-    }
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+    },
 });
